fd3bea792d74d7b97324e1e998ddb73dc8235815
Matei Istoan <matei.istoan@inria.fr>
2021-01-07 09:24:09 -0300

    removed merge-related errors from test_generatice.c

--- tests/test_absoluteError/test_generative.c
+++ tests/test_absoluteError/test_generative.c
@@ -8,14 +8,6 @@
 #include "../../src/common/float_struct.h"
 #include "../../src/common/float_utils.h"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#define DEBUG_MODE 0
-
-=======
->>>>>>> formatting
-=======
->>>>>>> ea03e4ae9991dd713ccc4ca8c7125a260a252fe1
 #define DEBUG_MODE 0
 
 double applyOp_double(char op, double a, double b) {
